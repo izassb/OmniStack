30e426e9df0ab73a547ac555ca46151050232775
izassb <izabelly.00000830234@unicap.br>
2022-01-18 16:26:56 -0300

    frontend 3

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,5 +1,12 @@
 
-import React, { useState } from "react"; 
+import React from "react"; 
+
+import './global.css'; 
+import './app.css'; 
+import './sidebar.css'; 
+
+//{ useState } 
+//useState: funçao criada pelo react p usar um estado
 
 // Componente: função que retorna algum conteúdo html/css/js (propriedades da página independentes - propriedades repetitivas) 
 // Bloco isolado de html, css e js; o qual não interfere no restante da aplicação 
@@ -7,6 +14,7 @@ import React, { useState } from "react";
 // função SEMPRE com a letra maiuscula 
 
 // Estado: informação que o componente vai manipular
+//Informações mantidas pelo componente(imutabilidade)
 
 // Propriedade: passar atributo pra um componente no react (titulo - header)
 // informações que um componente PAI passa para o componente filho
@@ -17,24 +25,66 @@ import React, { useState } from "react";
 function App() {
   //componente: App
 
-  const counter = useState(0);
+  //const [counter, setCounter] = useState(0);
   
+  // []desestruturação
   //let counter = 0;
   //let e nao const pq vai mudar o valor 
 
-  function incrementCounter() {
-    counter++;
-  }
+  //function incrementCounter() {
+    //setCounter(counter + 1);
+  //}
   //toda função que é propria de um componente a gente cria ela dentro dele mesmo
   
   return (
-    <>
-      <h1>Contador: {counter}</h1>
-      <button onClick={incrementCounter}>Incrementar</button>
-    </>
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div class="input-block">
+            <label htmlFor="github_username">Usuário do Github</label>
+            <input name="github_username" id="github_username" required />
+          </div>
+
+          <div class="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" required />
+          </div>
+
+          <div className="input-group">
+            <div class="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" required />
+            </div>
+
+            <div class="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input name="longitude" id="longitude" required />
+            </div>
+        
+          </div>
+        
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
+      <main>
+
+      </main>
+    </div>
+
+    //aside(sidebar)
+    //main(conteudo principal da aplicação)
+    //<>
+      //<h1>Contador: {counter}</h1>
+      //<button onClick={incrementCounter}>Incrementar</button>
+    //</>
     //variavel sendo utilizada no contador 
     //fragment"
   );
+
 }
+
+//global.css : estilizaçoes globais que eu quero que aplique em todo meu projeto
+//app.css : css de estilização diretamente do app.js
 
 export default App;
